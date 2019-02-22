582896df36690789bbf33755392e6a01728350b1
M Haidar Hanif <git@mhaidarhanif.com>
2019-02-22 14:22:50 +0700

    Improve code quality

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
+require('dotenv').config()
+
 const knex = require('knex')({
-  client: 'mysql',
+  client: process.env.DB_CLIENT || 'mysql',
   connection: {
-    host: 'localhost',
-    user: 'root',
-    password: 'baraboy86',
-    database: 'mytasks'
+    host: process.env.DB_HOST || 'your_host_here',
+    user: process.env.DB_USER || 'your_user_here',
+    password: process.env.DB_PASSWORD || 'your_password_here',
+    database: process.env.DB_DATABASE || 'your_database_here'
   }
 })
+
 knex('users').then(rows => {
   console.log(rows)
 })
